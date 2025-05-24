43c9bfb8c15e1e4d90ab5b4b15f301ea8eb5249d
Erick Perez <ericksperezc@gmail.com>
2025-05-23 21:44:35 -0500

    Add files via upload

---
+++ HolaMundo.c
@@ -0,0 +1,15 @@
+//Hola mundo
+
+#include<stdio.h> 
+/*Con el numeral hacemos el llamado, con el "Include" como su
+ palabra lo dice lo que hacemos es incluirlas, "std" significa 
+standar y "io" significa input and output y el .h es quien 
+guarda las librerias*/
+
+//Función Principal
+int main(){
+	//Mando a imprimir en la consola
+	printf("Hola mundo, soy Erick y esto es C");
+	//Retornamos 0 para confirmar que esta bien el codigo
+	return 0;
+}
