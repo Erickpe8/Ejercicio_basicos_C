853881a2ee51d2af84aca4cf8bf7c63b49406dcc
Erick Perez <ericksperezc@gmail.com>
2025-05-25 12:04:59 -0500

    Add files via upload

---
+++ Guardar_e_Imprimir_un_numero.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	// Vamos a guardar un número proporcionado por el usuario y luego lo imprimimos
+	int numeroUsuario;
+	
+	printf("\nProporciona un número:\n");
+	scanf("%d", &numeroUsuario);
+	
+	printf("\nEl número del usuario es: %d\n", numeroUsuario);  
+	
+	return 0;
+}
