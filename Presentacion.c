b35dbef3020f2a472835da5054b4bf7eb1e62fb7
Erick Perez <ericksperezc@gmail.com>
2025-05-25 10:46:08 -0500

    Add files via upload

---
+++ Presentacion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	// Declaramos e inicializamos las variables
+	int miEdad = 19;
+	char miNombre[] = "Erick";  
+	
+	// Imprimimos
+	printf("Hola, mi nombre es %s y tengo %d años.\n", miNombre, miEdad);
+	
+	return 0;
+}
