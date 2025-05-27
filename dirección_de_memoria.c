6d89220d69380b5bef4260d02355c656c1a2c7ea
Erick Perez <ericksperezc@gmail.com>
2025-05-26 20:46:08 -0500

    Add files via upload

---
+++ dirección_de_memoria.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+	// Primero declaramos las variables
+	int  miNumero = 10;
+	printf("imprimimos el valor de la variable miNumero: %d", miNumero);
+	printf("\nAhora imprimiremos la dirección de memoria: %p", &miNumero);
+	return 0;
+}
+
