fcf82a1da36948f06efcf70816faa56514c6b667
Erick Perez <ericksperezc@gmail.com>
2025-06-01 12:50:20 -0500

    Create Definir_constantes.c

---
+++ Definir_constantes.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	//Definimos las constantes
+	
+	const int MI_CONSTANTE = 10; //Como buena practica se le debe asignar un nombre a las constantes en mayusculas 
+	//Claramente las constantes no se pueden cambiar
+	
+	printf ("El valor de la constante es: %d", MI_CONSTANTE);
+	
+	return 0;
+}
+
